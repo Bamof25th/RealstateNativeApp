814d02a2530a58cb7bb0a21da6dc87180e64d5ec
Aniket Baghel <88363603+Bamof25th@users.noreply.github.com>
2024-12-25 22:49:40 +0530

    search component

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,19 +1,43 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold font-rubik text-primary-300   text-2xl">
-        Welcome to RealState
-      </Text>
-
-    </View>
+    <SafeAreaView className="h-full  bg-white">
+      <View className="px-5">
+        <View className=" flex flex-row items-center justify-between mt-5">
+          <View className=" flex flex-row ">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                John Doe
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+        {/* Search Section */}
+        <Search />
+        <View className="my-5">
+          {/* featured section */}
+          <View className="flex flex-row  items-center justify-between ">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
